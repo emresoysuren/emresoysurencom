1165dddd26f27ca60de0463ac02d1c777c44eae2
emresoysuren <emresoysuren03@gmail.com>
2023-01-14 02:07:56 +0300

    mobile optimization

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
 "icons/Icon-maskable-192.png": "4bf8c5d379227f92745fc48e0fc6ab6f",
 "icons/Icon-maskable-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
-"index.html": "b7dd65353bad5b716a287826efce8827",
-"/": "b7dd65353bad5b716a287826efce8827",
-"main.dart.js": "d7d5d620cc938a7643dd3d50e998f7b5",
+"index.html": "1b17b7e210b4a84394d24519427ac383",
+"/": "1b17b7e210b4a84394d24519427ac383",
+"main.dart.js": "338b2c1cc4ab734457c68ca1da7c5050",
 "manifest.json": "b787188a689fb020e2809e13653bf5b2",
 "version.json": "2045523fbd198ff2ba4ac7e09d9252e0"
 };
