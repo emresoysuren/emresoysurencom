0cae6ab12ba34c72d1470855a4dc7b105a001f1b
emresoysuren <emresoysuren03@gmail.com>
2023-01-14 14:53:29 +0300

    made boxes with wraps scrollable

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
 "icons/Icon-maskable-192.png": "4bf8c5d379227f92745fc48e0fc6ab6f",
 "icons/Icon-maskable-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
-"index.html": "853d04fa165fabce01cc6dfa2b835f27",
-"/": "853d04fa165fabce01cc6dfa2b835f27",
-"main.dart.js": "32a4dca8bf9cc9f80585bae85309e2d2",
+"index.html": "9d53e001eb5a3661bdb627d8c44e1b94",
+"/": "9d53e001eb5a3661bdb627d8c44e1b94",
+"main.dart.js": "97279920e132bc1bef89d0e49c7cebe8",
 "manifest.json": "b787188a689fb020e2809e13653bf5b2",
 "version.json": "2045523fbd198ff2ba4ac7e09d9252e0"
 };
