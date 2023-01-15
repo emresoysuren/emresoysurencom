c5b3244b41ab70390d649bc4a3142f67023e4dfe
emresoysuren <emresoysuren03@gmail.com>
2023-01-15 18:24:16 +0300

    made a widget for page control buttons

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
 "icons/Icon-maskable-192.png": "4bf8c5d379227f92745fc48e0fc6ab6f",
 "icons/Icon-maskable-512.png": "e0a4374a33838a93cd8f94464abc1cf7",
-"index.html": "3f4e9c3c4316c6989a3cd2b618f9bd5d",
-"/": "3f4e9c3c4316c6989a3cd2b618f9bd5d",
-"main.dart.js": "7e6f14883e32fee6bc4bef5d7757597c",
+"index.html": "f32adb9ebf9c5efbe6d83be9e0e46592",
+"/": "f32adb9ebf9c5efbe6d83be9e0e46592",
+"main.dart.js": "4ee6a43d501b91ef11ebca9c0bdbfd12",
 "manifest.json": "b787188a689fb020e2809e13653bf5b2",
 "version.json": "2045523fbd198ff2ba4ac7e09d9252e0"
 };
